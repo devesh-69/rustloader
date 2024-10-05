8722b1a48ee5cf15c3f4b76f7a45884cfa1fd4e7
devesh-69 <tatkaredevesh69@gmail.com>
2024-10-05 12:19:15 +0530

    vercel build update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/", // Adjust this if your app is in a subdirectory
   plugins: [react()],
   resolve: {
     alias: {
